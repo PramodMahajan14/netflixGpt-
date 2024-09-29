dc635999901eb44b2339934a6314675f942a1aa6
Pramod <pramodmahajan1411200@gmail.com>
2024-09-29 08:06:17 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-green-600 border-b-4 border-neutral-700">
+      <h1 className="text-2xl">Hello Pamod</h1>
+    </div>
+  );
+}
+
+export default App;
