ba660c8085f7946e2fb0cb57c8ae7bc39f010cba
Pramod <pramodmahajan1411200@gmail.com>
2024-11-11 13:39:14 +0530

    removed empty file

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { Provider } from "react-redux";
 import "./App.css";
 
 import Body from "./components/Body";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import { AppProvider } from "./context/AppProvider";
+
 import appStore from "./util/appStore";
 
 function App() {
