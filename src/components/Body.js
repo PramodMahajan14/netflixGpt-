f107df6f0d53426e68337100f717b00a13acd75a
Pramod <pramodmahajan1411200@gmail.com>
2024-11-14 23:57:37 +0530

    Changed Routes methods5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import SignIn from "./SignIn";
 import Browse from "./Browse";
 import ProtectedRoute from "../context/ProtectedRoute";
@@ -10,51 +16,73 @@ import PublicRoute from "../context/PublicRoute";
 
 import Home from "./Home";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <PublicRoute />,
-    children: [
-      {
-        path: "",
-        element: <SignIn />,
-      },
-    ],
-  },
-  {
-    path: "/browse",
-    element: <ProtectedRoute />,
-    children: [
-      {
-        element: <Browse />,
-        children: [
-          {
-            path: "",
-            index: true,
-            element: <Home />,
-          },
-          {
-            path: "search",
-            element: <SearchhMovies />,
-          },
-          {
-            path: "moviedetail/:MovieId",
-            element: <h1>hgj</h1>,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <h1>404 - Page Not Found</h1>,
-  },
-]);
+// const appRouter = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <PublicRoute />,
+//     children: [
+//       {
+//         path: "",
+//         element: <SignIn />,
+//       },
+//     ],
+//   },
+//   {
+//     path: "/browse",
+//     element: <ProtectedRoute />,
+//     children: [
+//       {
+//         element: <Browse />,
+//         children: [
+//           {
+//             path: "",
+//             index: true,
+//             element: <Home />,
+//           },
+//           {
+//             path: "search",
+//             element: <SearchhMovies />,
+//           },
+//           {
+//             path: "moviedetail/:MovieId",
+//             element: <h1>hgj</h1>,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     path: "*",
+//     element: <h1>404 - Page Not Found</h1>,
+//   },
+// ]);
 
 const Body = () => {
   return (
     <AuthProvider>
-      <RouterProvider router={appRouter} />
+      <BrowserRouter>
+        <Routes>
+          {/* Public route for sign-in */}
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<SignIn />} />
+          </Route>
+
+          {/* Protected route for authenticated pages */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/browse" element={<Browse />}>
+              <Route index element={<Home />} />
+              <Route path="search" element={<SearchhMovies />} />
+              <Route
+                path="moviedetail/:MovieId"
+                element={<h1>Movie Details</h1>}
+              />
+            </Route>
+          </Route>
+
+          {/* 404 fallback route */}
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 };
